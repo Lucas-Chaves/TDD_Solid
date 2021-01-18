04d13c54dc86b08a51465daab975d7ccfce9cfce
Lucas Chaves <lucas.limachaves@outlook.com>
2021-01-18 17:42:59 -0300

    ensure RemoteAuthentication calls HttpClient with correct Body

--- test/data/usecases/remote_authentication_test.dart
+++ test/data/usecases/remote_authentication_test.dart
@@ -1,5 +1,6 @@
 import 'package:faker/faker.dart';
 import 'package:mockito/mockito.dart';
+import 'package:tdd_solid_design/domain/usecases/usecases.dart ';
 import 'package:test/test.dart';
 import 'package:meta/meta.dart';
 
@@ -12,8 +13,9 @@ class RemoteAuthentication {
     @required this.url,
   });
 
-  Future<void> auth() async {
-    httpClient.request(url: url, method: 'post');
+  Future<void> auth(AuthenticationParams params) async {
+    final body = {'email': params.email, 'password': params.secret};
+    httpClient.request(url: url, method: 'post', body: body);
   }
 }
 
@@ -21,6 +23,7 @@ abstract class HttpClient {
   Future<void> request({
     @required String url,
     @required String method,
+    Map body,
   });
 }
 
@@ -40,13 +43,18 @@ void main() {
   });
 
   test('Should call HttpClient with correct values', () async {
+    final params = AuthenticationParams(
+      email: faker.internet.email(),
+      secret: faker.internet.password(),
+    );
+
     ///2º Act -> Executa o teste, chamando alguma função ou algo do tipo
-    await sut.auth();
+    await sut.auth(params);
 
     ///3º Assert -> Verifica se a operação realizada na anterior (Act) surtiu o resultado esperado
     verify(httpClient.request(
-      url: url,
-      method: 'post',
-    ));
+        url: url,
+        method: 'post',
+        body: {'email': params.email, 'password': params.secret}));
   });
 }
