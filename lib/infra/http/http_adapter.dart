67c438dbeb0ea53a89d71b157742d3918117a31f
Lucas Chaves <lucas.limachaves@outlook.com>
2021-02-09 15:35:35 -0300

    move duplicated code to a private method

--- lib/infra/http/http_adapter.dart
+++ lib/infra/http/http_adapter.dart
@@ -14,6 +14,10 @@ class HttpAdapter implements HttpClient {
     final headers = {'content-type': 'application/json', 'accept': 'application/json'};
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
+    return _handleResponse(response);
+  }
+
+  Map _handleResponse(Response response) {
     if (response.statusCode == 200) {
       return response.body.isEmpty ? null : jsonDecode(response.body);
     } else {
